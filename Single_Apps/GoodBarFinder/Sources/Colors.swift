209c77a46cd0ae3f94aaf2f9e3820e1a06ffb87a
Dylan Elliott <dylan.elliott@gmail.com>
2020-04-06 00:46:52 +1000

    Add bar finder

---
+++ Single_Apps/GoodBarFinder/Sources/Colors.swift
@@ -0,0 +1,8 @@
+import UIKit
+
+extension UIColor {
+    static let toolbar = UIColor(hex: "10171e")
+    static let h1 = UIColor(hex: "ffffff")
+    static let h2 = UIColor(hex: "ffffff")
+    static let background = UIColor(hex: "1b2632")
+}
