a1077cbd15a5b2d2ab08b91c828d8cd1b11c8b84
Dylan Elliott <dylan.elliott@gmail.com>
2021-04-22 20:04:25 +1000

    Cleanup Wayfinder home screen

--- Single_Apps/Wayfinder/Sources/Colors.swift
+++ Single_Apps/Wayfinder/Sources/Colors.swift
@@ -1,8 +1,8 @@
 import UIKit
 
 extension UIColor {
-    static let toolbar = UIColor(hex: "10171E")
-    static let h1 = UIColor(hex: "484747")
-    static let h2 = UIColor(hex: "1B2632")
-    static let background = UIColor(hex: "1B2632")
+    static let toolbar = UIColor(hex: "ffffff")
+    static let h1 = UIColor(hex: "000000")
+    static let h2 = UIColor(hex: "000000")
+    static let background = UIColor(hex: "ffffff")
 }
